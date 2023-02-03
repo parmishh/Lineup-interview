94c2d8fcd0613a492e9078c8b82e3bd8b6edcf97
paramjit singh <91942072+parmishh@users.noreply.github.com>
2023-02-04 00:39:58 +0530

    Update UploadRequest.js

--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -16,5 +16,5 @@ API.interceptors.request.use((req) => {
 
 export const uploadImage = (data) => API.post("/upload/", data);
 export const uploadVideo= (data) => API.post("/upload/", data);
-export const uploadPost = (data) => API.post("/posts", data);
+export const uploadPost = (data) => API.post("/posts",data);
  
